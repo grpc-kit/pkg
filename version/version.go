5687108bac725d75614c641f03411faa40fc420d
mingqing <mingqing@henji.org>
2020-04-13 18:13:38 +0800

    Added: 添加版本参数未设置时的默认值

--- version/version.go
+++ version/version.go
@@ -6,14 +6,14 @@ import (
 	"strconv"
 )
 
-// Get 用于转换编译时的静态信息，如版本号等
+// Get 用于转换编译时的静态信息，如版本号等，同时为未设置变量参数设置默认值（对于直接访问变量则无法提供默认值）
 func Get() Info {
 	cut, err := strconv.ParseInt(CommitUnixTime, 10, 64)
 	if err != nil {
 		cut = 0
 	}
 
-	return Info{
+	info := Info{
 		GitCommit:      GitCommit,
 		BuildDate:      BuildDate,
 		CommitUnixTime: cut,
@@ -24,4 +24,22 @@ func Get() Info {
 		Platform:       fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
 		CliVersion:     CliVersion,
 	}
+
+	if info.GitCommit == "" {
+		info.GitCommit = "1234567890123456789012345678901234567890"
+	}
+	if info.BuildDate == "" {
+		info.BuildDate = "1970-01-01T00:00:00Z"
+	}
+	if info.GitBranch == "" {
+		info.GitBranch = ""
+	}
+	if info.ReleaseVersion == "" {
+		info.ReleaseVersion = "v0.0.0"
+	}
+	if info.CliVersion == "" {
+		info.CliVersion = "v0.0.0"
+	}
+
+	return info
 }
