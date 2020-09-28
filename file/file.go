37740311e382e54da3a00606f4fd490335f5641f
mingqing <mingqing@henji.org>
2020-09-28 22:17:51 +0800

    style: 自定义模版函数名更改为驼峰风格

--- file/file.go
+++ file/file.go
@@ -57,11 +57,11 @@ func WriteString(path string, s string, perm os.FileMode) error {
 // ParseExecute 用于解析模版
 func ParseExecute(tmplStr string, data interface{}) (string, error) {
 	funcMap := template.FuncMap{
-		"title":      strings.Title,
-		"to_upper":   strings.ToUpper,
-		"to_lower":   strings.ToLower,
-		"to_title":   strings.ToTitle,
-		"trim_space": strings.TrimSpace,
+		"title":     strings.Title,
+		"toUpper":   strings.ToUpper,
+		"toLower":   strings.ToLower,
+		"toTitle":   strings.ToTitle,
+		"trimSpace": strings.TrimSpace,
 	}
 
 	tmpl, err := template.New("").Funcs(funcMap).Parse(tmplStr)
