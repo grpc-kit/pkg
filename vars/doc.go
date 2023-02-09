1e469d83a95f8c8ca7b3768fab56fbda5d8753f2
mingqing <mingqing@henji.org>
2023-02-09 10:43:51 +0800

    refactor: 更改 version 目录为 vars
    为了兼容当前目录下创建 VERSION 文件记录版本号，而把原先 "version" 重
    新命名为 "vars"，对原因引用会存在破坏性，需更改引用路径。
    
    BREAKING CHANGE: 更改包名为 vars
    
    涉及以下几个文件
    
    1. Makefile
    2. cmd/server/main.go

---
+++ vars/doc.go
@@ -0,0 +1,3 @@
+// Package vars 提供统一的功能变量，如版本信息
+
+package vars
---
+++ vars/version.go
@@ -0,0 +1,46 @@
+package vars
+
+import (
+	"fmt"
+	"runtime"
+	"strconv"
+)
+
+// GetVersion 用于转换编译时的静态信息，如版本号等，同时为未设置变量参数设置默认值（对于直接访问变量则无法提供默认值）
+func GetVersion() Info {
+	cut, err := strconv.ParseInt(CommitUnixTime, 10, 64)
+	if err != nil {
+		cut = 0
+	}
+
+	info := Info{
+		Appname:        Appname,
+		GitCommit:      GitCommit,
+		BuildDate:      BuildDate,
+		CommitUnixTime: cut,
+		GitBranch:      GitBranch,
+		ReleaseVersion: ReleaseVersion,
+		GoVersion:      runtime.Version(),
+		Compiler:       runtime.Compiler,
+		Platform:       fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		CliVersion:     CliVersion,
+	}
+
+	if info.GitCommit == "" {
+		info.GitCommit = "1234567890123456789012345678901234567890"
+	}
+	if info.BuildDate == "" {
+		info.BuildDate = "1970-01-01T00:00:00Z"
+	}
+	if info.GitBranch == "" {
+		info.GitBranch = ""
+	}
+	if info.ReleaseVersion == "" {
+		info.ReleaseVersion = "0.0.0"
+	}
+	if info.CliVersion == "" {
+		info.CliVersion = "0.0.0"
+	}
+
+	return info
+}
