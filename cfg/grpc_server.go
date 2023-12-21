d77e26adb46ec58303fcd74a46e7a8c8dee66e94
mingqing <mingqing@henji.org>
2023-12-21 22:48:51 +0800

    refactor: 针对 grpc 转换的 http 接口强制以 /api 为前缀

--- cfg/grpc_server.go
+++ cfg/grpc_server.go
@@ -323,8 +323,8 @@ func (c *LocalConfig) getHTTPServeMux(customOpts ...runtime.ServeMuxOption) (*ht
 	handler = c.HTTPHandler(handler)
 
 	// TODO；后续如需集成前端，可考虑添加 "/api" 前缀，把 ”/“ 存放静态 HTML
-	hmux.Handle("/", handler)
-	// hmux.Handle("/api/", handler)
+	// hmux.Handle("/", handler)
+	hmux.Handle("/api/", handler)
 
 	return hmux, rmux
 }
