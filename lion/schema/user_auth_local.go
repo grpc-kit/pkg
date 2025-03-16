7748b72d155a11db8dcd9b42d25ea7e9aa0fd960
mingqing <mingqing@henji.org>
2025-03-16 23:07:22 +0800

    feat: 添加 lion_user_auth_local 模型设计

---
+++ lion/schema/user_auth_local.go
@@ -0,0 +1,63 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"entgo.io/ent/schema/mixin"
+)
+
+// UserAuthLocal 存储本地用户的认证信息（如密码）
+type UserAuthLocal struct {
+	ent.Schema
+}
+
+// Fields of the table.
+func (UserAuthLocal) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("user_id").
+			Unique().
+			Positive().
+			Immutable().
+			Comment("用户ID，关联 lion_users 表"),
+		field.Bytes("password_encrypted").
+			Sensitive().
+			NotEmpty().
+			Comment("加密后的密码"),
+		field.Bool("mfa_enabled").
+			Default(false).
+			Comment("是否启用 MFA"),
+		field.Bytes("mfa_secret_encrypted").
+			Sensitive().
+			Default([]byte("")).
+			Comment("加密后的 MFA 密钥"),
+		field.Time("password_changed_at").
+			Optional().
+			Nillable().
+			Comment("密码最后一次更改时间"),
+		field.Time("password_expires_at").
+			Optional().
+			Nillable().
+			Comment("密码过期时间"),
+	}
+}
+
+// Edges of the table.
+func (UserAuthLocal) Edges() []ent.Edge {
+	return nil
+}
+
+// Mixin of the table.
+func (UserAuthLocal) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		mixin.Time{},
+	}
+}
+
+// Annotations 自定义表名
+func (UserAuthLocal) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "lion_user_auth_local"},
+	}
+}
