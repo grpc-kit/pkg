eaa2e87eb3f617b533d4e937d76e1356cc638009
mingqing <mingqing@henji.org>
2025-09-11 23:36:07 +0800

    chore(lion): add lion_role_permissions demo

---
+++ lion/schema/role_permissions.go
@@ -0,0 +1,46 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/dialect/entsql"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+)
+
+// RolePermissions holds the schema definition for the RolePermissions entity.
+type RolePermissions struct {
+	ent.Schema
+}
+
+// Fields of the table.
+func (RolePermissions) Fields() []ent.Field {
+	return []ent.Field{
+		field.Int("role_id").
+			Positive().
+			// Immutable().
+			Comment("关联 lion_roles 表的角色 ID"),
+		field.Int("permission_id").
+			Positive().
+			// Immutable().
+			Comment("关联 lion_permissions 表的菜单 ID"),
+	}
+}
+
+// Edges of the table.
+func (RolePermissions) Edges() []ent.Edge {
+	return nil
+}
+
+// Mixin of the table.
+func (RolePermissions) Mixin() []ent.Mixin {
+	return []ent.Mixin{
+		TimeMixinWithoutDeleted{},
+	}
+}
+
+// Annotations 自定义表名
+func (RolePermissions) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entsql.Annotation{Table: "lion_role_permissions"},
+	}
+}
