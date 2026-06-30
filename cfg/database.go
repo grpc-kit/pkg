a6bea16e2d7b11b523a44441edde6d417f4df90d
mingqing <mingqing@henji.org>
2026-06-30 22:24:30 +0800

    feat: 移除过时的用户成员资格迁移逻辑

--- cfg/database.go
+++ cfg/database.go
@@ -170,9 +170,6 @@ func (c *LocalConfig) GetAdminDatabaseLion() (*lion.Client, error) {
 	if err = db.Schema.Create(context.TODO()); err != nil {
 		c.logger.Warnf("lion migrate err: %v", err)
 	}
-	if err = backfillLegacyLionUserMemberships(context.TODO(), rawDB, c.Database.Driver); err != nil {
-		return nil, err
-	}
 
 	return db, nil
 }
